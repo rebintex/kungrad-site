9b0bdcd7e8cb0683072f04f2659a44458278f4d0
rebintex <rebintex@mail.ru>
2021-06-12 22:52:25 +0500

    index page first

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// function initMap() {
+
+//     const macc = {lat: 43.235495049984706, lng: 58.842033815717784};
+//     let map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 15, center: macc});
+
+//     let marker = new google.maps.Marker({position: macc, map: map});
+
+//   }
+//   43.235495049984706, 58.842033815717784
